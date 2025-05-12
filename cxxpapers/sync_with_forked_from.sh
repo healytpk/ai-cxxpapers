9c1a6fa6c9dc9810de3074d87e4eb2cf5fce77c4
healytpk <thomaspkhealy@yahoo.com>
2025-05-12 15:12:43 +0100

    In script of synchronsing repo, write obvious banner at end

--- cxxpapers/sync_with_forked_from.sh
+++ cxxpapers/sync_with_forked_from.sh
@@ -2,4 +2,8 @@
 git remote -v
 git fetch forked_from
 git rebase forked_from/master
+echo
+echo
+echo ===========================================================================================
 echo "Now you need to type: git push -f"
+echo ===========================================================================================
