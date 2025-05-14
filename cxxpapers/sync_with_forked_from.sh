2dd6e24ab15b935516bbf129896fcc887fdb1ae8
healytpk <thomaspkhealy@yahoo.com>
2025-05-14 12:49:01 +0100

    Change to script to sync with original repo

--- cxxpapers/sync_with_forked_from.sh
+++ cxxpapers/sync_with_forked_from.sh
@@ -5,5 +5,5 @@ git rebase forked_from/master
 echo
 echo
 echo ===========================================================================================
-echo "Now you need to type: git push -f"
+echo "Now you need to type: git push -f"        !!!!! but only if rebase was uneventful !!!!!
 echo ===========================================================================================
