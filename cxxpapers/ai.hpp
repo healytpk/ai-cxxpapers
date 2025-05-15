e513ffe453f88b13c80213599761d2d919b39c3a
healytpk <thomaspkhealy@yahoo.com>
2025-05-15 16:22:28 +0100

    AI probably working but just too slow to confirm

--- cxxpapers/ai.hpp
+++ cxxpapers/ai.hpp
@@ -3,6 +3,8 @@
 
 #include <cstdint>                     // int32_t
 #include <mutex>                       // mutex
+#include <string>                      // string
+#include <string_view>                 // string_view
 #include <vector>                      // vector
 
 struct llama_context;
@@ -12,6 +14,8 @@ struct llama_vocab;
 
 class AImanager {
 protected:
+    static constexpr int n_predict = 32;
+
     llama_model       *model = nullptr;
     llama_sampler     *smpl  = nullptr;
     llama_context     *ctx   = nullptr;
@@ -20,17 +24,20 @@ protected:
     int m_ngl   =   99;
     int m_n_ctx = 2048;
 
-    std::mutex mtx;
+    std::recursive_mutex mtx;
 
 public:
 
+    void Reset(void);
     ~AImanager(void) noexcept;
 
-    bool Init(void);
-    void Reset(void);
-    int OtherFunction(void);
-    void ForgetEverything(void) noexcept;
-    void LoadInTokens(std::vector<std::int32_t> const *);
+    void Init(void) noexcept(false);
+
+    void DiscardContext(void) noexcept;
+    void NewContext(std::size_t) noexcept(false);
+    void LoadInPaper(std::vector<std::int32_t> const *) noexcept(false);
+    std::string PutInAndGetOut( std::vector<std::int32_t> const & ) noexcept(false);
+    std::string PutInAndGetOut( std::string_view ) noexcept(false);
 };
 
 #endif
